0b8e197b4a3b4ea611ceaa0dd8344379115fae8a
GUIGUI <guillaume.marcelino@efrei.net>
2022-10-25 15:41:57 +0200

    connect routes

--- src/routes/api/secured/index.ts
+++ src/routes/api/secured/index.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 
 import users from "./users";
+import beers from "./beers";
+import reviews from "./reviews";
+import types from "./types";
 
 const api = Router();
 
 api.use("/users", users);
+api.use("/beers", beers);
+api.use("/reviews", reviews);
+api.use("/types", types);
+
 
 export default api;
